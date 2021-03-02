f4b07b8e097e898d464c0133241c9596bc13b7d8
seolleung2 <seolleung22@gmail.com>
2021-03-03 01:41:51 +0900

    [Fix] 로그인 및 회원가입창, 마이페이지 내 DOM 요소 직접선택코드 수정 - useRef 사용, DOM 요소 직접 선택 하지 않게 코드 개선작업 진행. - 로그인, 회원가입 창 전환되는 버튼과 관련된 요소에 대한 개선.

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -6,14 +6,6 @@ import Schedules from '../components/Schedules';
 import Footer from '../components/Footer';
 
 const MyPage = () => {
-  const onScroll = () => {
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-  };
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
   return (
     <div className="wrapper">
       <Header />
--- src/pages/SignInUp.js
+++ src/pages/SignInUp.js
@@ -1,32 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 import SocialLogin from '../components/SocialLogin';
 
-import logo1 from '../assets/images/logo1.png';
-
 const SignInUp = () => {
-  const onClickHandler = () => {};
-  const onToggle = () => {
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+  const containerRef = useRef(null);
+  const signUpBtnRef = useRef(null);
+  const signInBtnRef = useRef(null);
 
-    signUpButton.addEventListener('click', () => {
-      container.classList.add('right-panel-active');
+  const onToggle = () => {
+    signUpBtnRef.current.addEventListener('click', () => {
+      containerRef.current.classList.add('right-panel-active');
     });
-    signInButton.addEventListener('click', () => {
-      container.classList.remove('right-panel-active');
+    signInBtnRef.current.addEventListener('click', () => {
+      containerRef.current.classList.remove('right-panel-active');
     });
   };
+
   useEffect(() => {
     window.addEventListener('click', onToggle);
     return () => window.removeEventListener('click', onToggle);
-  }, []);
+  }, [containerRef, signInBtnRef, signUpBtnRef]);
 
   return (
     <div className="wrapper-container">
-      <div className="container" id="container">
+      <div className="container" id="container" ref={containerRef}>
         <SignUp />
         <SignIn />
         <div className="overlay-container">
@@ -34,14 +32,14 @@ const SignInUp = () => {
             <div className="overlay-panel overlay-left">
               <h1>Already Have an Account?</h1>
               <p>나간다를 즐길 준비가 되셨나요? 로그인하러 갑시다!</p>
-              <button className="ghost" id="signIn">
+              <button className="ghost" id="signIn" ref={signInBtnRef}>
                 Sign In
               </button>
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Don&apos;t have an Account?</h1>
               <p>이메일과 비밀번호 만으로 간편한 가입을 할 수 있습니다.</p>
-              <button className="ghost" id="signUp" onClick={onClickHandler}>
+              <button className="ghost" id="signUp" ref={signUpBtnRef}>
                 Sign Up
               </button>
               <SocialLogin />
