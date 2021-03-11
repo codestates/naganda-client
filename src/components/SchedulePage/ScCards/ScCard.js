53831aa7646eafe60be62ac3f0a92c467d8f3b06
seolleung2 <seolleung22@gmail.com>
2021-03-11 11:50:26 +0900

    [Fix] 리액트 스케줄러 컴포넌트 트리 재설정, DnD 기능 적용 - 스케줄러 컴포넌트 단순화 - React-beautiful-dnd 설치(with lodash, uuid) - Drag and Drop 사용 가능 - 현재 코드에서는 상태 저장을 적용하지 않음 - 리덕스 코드 적용 예정

--- src/components/SchedulePage/ScCards/ScCard.js
+++ src/components/SchedulePage/ScCards/ScCard.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
-const ScCard = ({ type, detailTitle, time, place, comment }) => {
+const ScCard = ({ id, index, detailTitle, type, time, place, comment }) => {
   return (
-    <div className="sc-card">
-      <div className="card-infos">
-        <div className="card-info">
-          <h4>{detailTitle}</h4>
-          <span className="time">
-            Estimated time:{type} {time}
-          </span>
+    <Draggable draggableId={String(id)} index={index}>
+      {(provided) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="sc-card"
+        >
+          <div className="card-infos">
+            <div className="card-info">
+              <h4>{detailTitle}</h4>
+              <span className="time">
+                Estimated time: {type} {time}
+              </span>
+            </div>
+            <span className="btn-closed">
+              <i className="far fa-trash-alt"></i>
+            </span>
+          </div>
+          <div className="text-content">{comment}</div>
+          <div className="address-info">
+            <h4>{place}</h4>
+            <i className="fas fa-map-marker-alt"></i>
+          </div>
         </div>
-        <span className="btn-closed">
-          <i className="far fa-trash-alt"></i>
-        </span>
-      </div>
-      <div className="text-content">{comment}</div>
-      <div className="address-info">
-        <h4>{place}</h4>
-        <i className="fas fa-map-marker-alt"></i>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
--- src/components/mock/ScheduleListData.js
+++ src/components/mock/ScheduleListData.js
@@ -1,90 +1,115 @@
+import { v4 as uuid } from 'uuid';
+
 const scheduleListData = [
   {
-    id: 1,
-    type: 'am',
-    detailTitle: 'Go and eat Taco🌮🌮',
-    time: '10:00',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `I’m going to Mexican Restaurant at ten o’clock this morning🌻. Not
-    lonely at all because I’m going with friends 🚗🚗Danbi, Suhyun and
-    Junghwan are all nice people!`,
+    id: `list-${0}`,
+    title: '🌳Morning',
+    cards: [
+      {
+        id: uuid(),
+        type: 'am',
+        detailTitle: 'Go and eat Taco🌮🌮',
+        time: '10:00',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `I’m going to Mexican Restaurant at ten o’clock this morning🌻. Not
+      lonely at all because I’m going with friends 🚗🚗Danbi, Suhyun and
+      Junghwan are all nice people!`,
+      },
+      {
+        id: uuid(),
+        type: 'am',
+        detailTitle: 'Starbucks Coffee☕️☕️',
+        time: '11:00',
+        place: '1F, Sehwa Building 889-40 Daechi-Dong',
+        comment: `It’s surprising how different brewing methods can enhance particular characteristics in your coffee.
+      Let us help you unlock the full potential of your coffee—for the perfect cup every time.`,
+      },
+    ],
   },
   {
-    id: 2,
-    type: 'am',
-    detailTitle: 'Starbucks Coffee☕️☕️',
-    time: '11:00',
-    place: '1F, Sehwa Building 889-40 Daechi-Dong',
-    comment: `It’s surprising how different brewing methods can enhance particular characteristics in your coffee.
-    Let us help you unlock the full potential of your coffee—for the perfect cup every time.`,
-  },
-  {
-    id: 3,
-    type: 'pm',
-    detailTitle: 'Eat Sashimi Rolls🍣🍣',
-    time: '14:00',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `The first difference is that sashimi is thinly sliced raw meat, typically fish that is served without rice.
+    id: `list-${1}`,
+    title: '🍄Afternoon',
+    cards: [
+      {
+        id: uuid(),
+        type: 'pm',
+        detailTitle: 'Eat Sashimi Rolls🍣🍣',
+        time: '14:00',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `The first difference is that sashimi is thinly sliced raw meat, typically fish that is served without rice.
     Typically, sashimi is some type of salmon or tuna. Other popular types of sashimi are mackerel, yellowtail, shrimp, scallops, clams and octopus.
     Translated, sashimi means “pierced fish."`,
-  },
-  {
-    id: 4,
-    type: 'pm',
-    detailTitle: 'Eat Sashimi Rolls🍣🍣',
-    time: '14:00',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `The first difference is that sashimi is thinly sliced raw meat, typically fish that is served without rice.
+      },
+      {
+        id: uuid(),
+        type: 'pm',
+        detailTitle: 'Eat Sashimi Rolls🍣🍣',
+        time: '14:00',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `The first difference is that sashimi is thinly sliced raw meat, typically fish that is served without rice.
     Typically, sashimi is some type of salmon or tuna. Other popular types of sashimi are mackerel, yellowtail, shrimp, scallops, clams and octopus.
     Translated, sashimi means “pierced fish."`,
+      },
+      {
+        id: uuid(),
+        type: 'pm',
+        detailTitle: 'Get Some Drinks🍺🍺',
+        time: '17:00',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `These are terms that can applied to any style of beer, not just IPAs. For instance, you can have a session West Coast IPA and a session Belgian IPA.`,
+      },
+    ],
   },
   {
-    id: 5,
-    type: 'pm',
-    detailTitle: 'Get Some Drinks🍺🍺',
-    time: '17:00',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `These are terms that can applied to any style of beer, not just IPAs. For instance, you can have a session West Coast IPA and a session Belgian IPA.`,
+    id: `list-${2}`,
+    title: '🌝Midnight',
+    cards: [
+      {
+        id: uuid(),
+        type: 'mid',
+        detailTitle: 'Wine and Pizza🍷🍕',
+        time: '23:00',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+      },
+      {
+        id: uuid(),
+        type: 'mid',
+        detailTitle: 'Chicken Party🐔🍗',
+        time: '23:30',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+      },
+      {
+        id: uuid(),
+        type: 'mid',
+        detailTitle: 'Soju & Vodka🍹🍹',
+        time: '23:40',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+      },
+      {
+        id: uuid(),
+        type: 'mid',
+        detailTitle: '밤에 뭐먹으면 살찐다😋😋',
+        time: '23:45',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+      },
+      {
+        id: uuid(),
+        type: 'mid',
+        detailTitle: '좋은 개발자가 되고 싶다👨🏻‍💻👨🏻‍💻',
+        time: '23:55',
+        place: 'Yeongdongdaero, 622, Samsung - 1 dong',
+        comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+      },
+    ],
   },
   {
-    id: 6,
-    type: 'mid',
-    detailTitle: 'Wine and Pizza🍷🍕',
-    time: '23:00',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
-  },
-  {
-    id: 7,
-    type: 'mid',
-    detailTitle: 'Chicken Party🐔🍗',
-    time: '23:30',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
-  },
-  {
-    id: 8,
-    type: 'mid',
-    detailTitle: 'Soju & Vodka🍹🍹',
-    time: '23:40',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
-  },
-  {
-    id: 9,
-    type: 'mid',
-    detailTitle: '밤에 뭐먹으면 살찐다😋😋',
-    time: '23:45',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
-  },
-  {
-    id: 10,
-    type: 'mid',
-    detailTitle: '좋은 개발자가 되고 싶다👨🏻‍💻👨🏻‍💻',
-    time: '23:55',
-    place: 'Yeongdongdaero, 622, Samsung - 1 dong',
-    comment: `A great Neapolitan pizza has the best sauce, the finest mozzarella, and maybe a few whole basil leaves. But before you can get into the toppings, you'll need to make the perfect crust.`,
+    id: `list-${3}`,
+    title: '👀Empty',
+    cards: [],
   },
 ];
 
