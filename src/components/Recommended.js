cc25a5b3edbc041417fb893b304dfe1c4f558938
seolleung2 <seolleung22@gmail.com>
2021-03-23 00:40:57 +0900

    [Fix] 랜딩페이지 레이아웃 수정 - 좌우 padding 을 통해 꽉찬 느낌을 해소 - line-height 로 자간 조절 - 글자 크기, 여백 조절 및 정렬

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -4,7 +4,7 @@ const Recommended = () => {
   return (
     <section className="recommend">
       <div className="recommend-box">
-        <h2>
+        <h2 className="reco-title">
           당신이 원하는 스케줄, <br />
           <span>바로 이 곳에👇🏻</span>
         </h2>
@@ -15,11 +15,11 @@ const Recommended = () => {
           <div className="item"></div>
         </div>
         <div className="hashtags hide2">
-          <div className="tag">#한옥마을🇰🇷</div>
+          <div className="tag">#한옥마을</div>
           <div className="tag">#경복궁🏰</div>
-          <div className="tag">#미슐랭3✨</div>
+          <div className="tag">#미슐랭✨</div>
           <div className="tag">#하동관🍚</div>
-          <div className="tag">#순대국밥🍲</div>
+          <div className="tag">#순대국밥</div>
         </div>
       </div>
       <div className="selected-box">
@@ -27,25 +27,25 @@ const Recommended = () => {
           <div className="selected-item">
             <h2 className="click-hashtag">
               <span className="desc-hashtag">
-                Let&apos;s Click HashTag on the Right side!🏷
+                Click HashTag on the Right side!
               </span>
             </h2>
           </div>
           <div className="hashtags">
-            <div className="tag">#한옥마을🇰🇷</div>
+            <div className="tag">#한옥마을</div>
             <div className="tag">#경복궁🏰</div>
-            <div className="tag">#미슐랭3✨</div>
+            <div className="tag">#미슐랭✨</div>
             <div className="tag">#하동관🍚</div>
-            <div className="tag">#순대국밥🍲</div>
+            <div className="tag">#순대국밥</div>
           </div>
         </div>
       </div>
       <div className="hashtags hide">
-        <div className="tag">#한옥마을🇰🇷</div>
+        <div className="tag">#한옥마을</div>
         <div className="tag">#경복궁🏰</div>
-        <div className="tag">#미슐랭3✨</div>
+        <div className="tag">#미슐랭✨</div>
         <div className="tag">#하동관🍚</div>
-        <div className="tag">#순대국밥🍲</div>
+        <div className="tag">#순대국밥</div>
       </div>
     </section>
   );
